2f6bc1de459a36bdbcc3aa4f27b5b73f1ae0fff4
Trishna Patel <trishnamorar@gmail.com>
2023-03-22 13:34:48 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Weather from "./Weather";
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
